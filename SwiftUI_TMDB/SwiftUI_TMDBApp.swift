d468e0b7f33df58fc369fc6d12105394141155f9
PelindungGiawa <61076880+PelindungGiawa@users.noreply.github.com>
2021-12-22 10:15:15 +0700

    Initial Commit

---
+++ SwiftUI_TMDB/SwiftUI_TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TMDBApp.swift
+//  SwiftUI_TMDB
+//
+//  Created by Pelindung Giawa on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
